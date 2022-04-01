512810369a6ffb3b0b38dc2e2ca26f5ced683501
joskalenda <joskalenda3@gmail.com>
2022-04-01 20:22:46 +0200

    Add enumerable and create  mylist class

---
+++ enumerable.rb
@@ -0,0 +1,16 @@
+# IMPORT module here
+
+class MyList
+
+  include MyEnumerable
+
+  def initialize(*list)
+    @list = list
+  end
+  
+  def each(&block)
+    @list.each(&block)
+  end
+end
+
+# add script test here
