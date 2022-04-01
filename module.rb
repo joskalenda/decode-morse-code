1110536b9e13dfc3e8b03b5dc1c1e29e24b12cb1
joskalenda <joskalenda3@gmail.com>
2022-04-01 20:04:59 +0200

    Create method for any?

--- module.rb
+++ module.rb
@@ -4,3 +4,10 @@ module MyEnumerable
     each {|i| compute = false unless block.yield(i) }
     compute
   end
+
+  def any?(&block)
+    compute = false
+    each { |i| compute = true if block.yield(i) }
+    compute
+  end
+
